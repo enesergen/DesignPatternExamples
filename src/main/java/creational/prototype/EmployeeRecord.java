f88191fec88efa8686b1b839eb0a999722532452
enes <eenes44@gmail.com>
2023-07-27 09:37:29 +0300

    Prototype Design pattern employee clone example

---
+++ src/main/java/creational/prototype/EmployeeRecord.java
@@ -0,0 +1,35 @@
+package creational.prototype;
+
+public class EmployeeRecord implements Prototype {
+    int id;
+    String name;
+    String designation;
+    double salary;
+    String address;
+
+    public EmployeeRecord() {
+        System.out.println("   Employee Records of Oracle Corporation ");
+        System.out.println("---------------------------------------------");
+        System.out.println("Eid" + "\t" + "Ename" + "\t" + "Edesignation" + "\t" + "Esalary" + "\t\t" + "Eaddress");
+    }
+
+    public EmployeeRecord(int id, String name, String designation, double salary, String address) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.address = address;
+    }
+
+    public void showRecord() {
+        System.out.println(id + "\t" + name + "\t" + designation + "\t" + salary + "\t" + address);
+
+    }
+
+    @Override
+    public Prototype getClone() {
+        return new EmployeeRecord(id,name,designation,salary,address);
+    }
+
+
+}
